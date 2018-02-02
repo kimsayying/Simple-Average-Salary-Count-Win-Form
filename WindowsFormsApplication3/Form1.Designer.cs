444bfe005d8bd78ce7987de261ce00cdfa7e61f5
sykim <kimsayying93@gmail.com>
2018-02-02 13:56:10 +0800

    Add project files.

---
+++ WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtinput1 = new System.Windows.Forms.TextBox();
+            this.txtInput2 = new System.Windows.Forms.TextBox();
+            this.msgLabel = new System.Windows.Forms.Label();
+            this.btnAverage = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtinput1
+            // 
+            this.txtinput1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtinput1.Location = new System.Drawing.Point(69, 30);
+            this.txtinput1.Name = "txtinput1";
+            this.txtinput1.Size = new System.Drawing.Size(201, 49);
+            this.txtinput1.TabIndex = 0;
+            // 
+            // txtInput2
+            // 
+            this.txtInput2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput2.Location = new System.Drawing.Point(422, 29);
+            this.txtInput2.Name = "txtInput2";
+            this.txtInput2.Size = new System.Drawing.Size(204, 49);
+            this.txtInput2.TabIndex = 1;
+            // 
+            // msgLabel
+            // 
+            this.msgLabel.AutoSize = true;
+            this.msgLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.msgLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.msgLabel.Location = new System.Drawing.Point(199, 182);
+            this.msgLabel.Name = "msgLabel";
+            this.msgLabel.Size = new System.Drawing.Size(0, 42);
+            this.msgLabel.TabIndex = 3;
+            this.msgLabel.Click += new System.EventHandler(this.msgLabel_Click);
+            // 
+            // btnAverage
+            // 
+            this.btnAverage.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAverage.Location = new System.Drawing.Point(244, 109);
+            this.btnAverage.Name = "btnAverage";
+            this.btnAverage.Size = new System.Drawing.Size(248, 49);
+            this.btnAverage.TabIndex = 4;
+            this.btnAverage.Text = "Average";
+            this.btnAverage.UseVisualStyleBackColor = true;
+            this.btnAverage.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(897, 262);
+            this.Controls.Add(this.btnAverage);
+            this.Controls.Add(this.msgLabel);
+            this.Controls.Add(this.txtInput2);
+            this.Controls.Add(this.txtinput1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtinput1;
+        private System.Windows.Forms.TextBox txtInput2;
+        private System.Windows.Forms.Button btnAverage;
+        private System.Windows.Forms.Label msgLabel;
+    }
+}
+
---
+++ WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            try
+            {
+                double average;
+                double input1 = double.Parse(txtinput1.Text);
+                double input2 = double.Parse(txtInput2.Text);
+
+                average = (input1 + input2) / 2;
+                msgLabel.BackColor = Color.Azure;
+
+                // Currency globalization
+                // This code- (sets currency to RM(Malaysia) then prints a line.)
+                // :C has the effect of formatting average value as currency, the default value is $(USD).
+                Thread.CurrentThread.CurrentCulture = new CultureInfo("en-MY", false);
+                msgLabel.Text = $"Average Salary: {average:C}";
+            }
+
+            catch
+            {
+                Match match = Regex.Match(txtinput1.Text, "^[a-zA-Z]*$");
+                Match match2 = Regex.Match(txtInput2.Text, "^[a-zA-Z]*$");
+               
+                if (txtinput1.Text == "" ||txtInput2.Text == "" || match.Success == true ||match2.Success == true)
+                {
+                    msgLabel.BackColor = Color.Red;
+                    msgLabel.Text = "Please enter a valid integer.";
+                }
+            }
+
+        }
+
+        private void msgLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
